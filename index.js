d87bb20a0895efc02d79869d4de3f74c95819d62
ma9da <ma9da.ahmed@gmail.com>
2021-10-09 21:07:26 +0200

    magda's functions added

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ console.log("hi there");
 
 // ahmed's 1st fun
 const ahmed1 = () => {
-    console.log("1st func")
-}
+	console.log("1st func");
+};
 ahmed1();
 
 // ahmed's 2nd fun
 const ahmed2 = () => {
-    console.log("1st func")
-}
+	console.log("1st func");
+};
 ahmed2();
+//magda 1st function
+function magda1() {
+	console.log("hi, from magda function one");
+}
+//magda 2nd function
+function magda2() {
+	console.log("hi, from magda function two");
+}
